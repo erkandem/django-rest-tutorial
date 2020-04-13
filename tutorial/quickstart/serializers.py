d80876367659e66621d73a9a34e2f0447792eaa5
kan <erkan.dem@pm.me>
2020-04-13 13:28:09 +0200

    applied quickstart

---
+++ tutorial/quickstart/serializers.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    """
+    from HypelinkedModelSerializer:
+
+        * A 'url' field is included instead of the 'id' field.
+        * Relationships to other instances are hyperlinks, instead of primary keys.
+
+    """
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'groups']
+
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = ['url', 'name']
--- tutorial/urls.py
+++ tutorial/urls.py
@@ -12,10 +12,24 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+This version of the quickstart within the  django rest framework
+seems to match django 1.11 syntax
+
+https://github.com/encode/django-rest-framework/blob/e148637d6d15d07562e3588b86d9ba94314150e0/docs/tutorial/quickstart.md
+
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from .quickstart import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'group', views.GroupViewSet)
+
 
 urlpatterns = [
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^admin/', admin.site.urls),
 ]
