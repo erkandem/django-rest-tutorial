45e61623c682bd34ca6601281d6e24591a6f4fda
kan <erkan.dem@pm.me>
2020-05-03 19:48:04 +0200

    dynamic querset via `get_querset`

--- options_data/views.py
+++ options_data/views.py
@@ -29,7 +29,39 @@ class ICEOptionRawDataViewSet(OptionRawDataViewSet):
 
 
 class USETFOptionRawDataViewSet(OptionRawDataViewSet):
-    queryset = OptionRawDataModel.objects.filter(
-        exchange__exact='USETF'
-    )
 
+    def get_queryset(self):
+        """
+        instead of defining a queryset which may or may not implement
+        filtering it is possible to use dynamic filtering by overwriting the
+        `get_queryset` method.
+
+        Access query parameters:
+
+            from rest_framework import generics
+            class SomeView(generics.ListAPIView):
+                def get_queryset(self)
+                    username = self.request.query_params.get('username', None)
+                    queryset = ModelOfSomeView.filter(username__exact=username)
+                    return queryset
+
+        Access via URL element:
+            # urls.py
+            url('^purchases/(?P<username>.+)/$', SomeView),
+
+            # views.py
+            from rest_framework import generics
+            class SomeView(generics.ListAPIView):
+                def get_queryset(self)
+                    username = self.kwarg['username']
+                    return ModelOfSomeView.filter(username__exact=username)
+
+        query parameter is designed to be optional (hence the .get() method)
+        Whereas the URL segment is expected to be certainly present (hence the ['key'] access)
+
+        also: django_filters package
+        """
+        queryset = OptionRawDataModel.objects.filter(
+            exchange__exact='USETF'
+        )
+        return queryset
